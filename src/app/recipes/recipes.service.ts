da699d64b52ed7aa493d1eb718d9f22ac532cfbc
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2022-12-14 14:10:15 +0100

    Another commit

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -29,4 +29,8 @@ export class RecipesService {
   getRecipe(id: string) {
     return {...this.recipes.find(recipe => recipe.id === id)}
   }
+
+  deleteRecipe(id: string) {
+    this.recipes = this.recipes.filter(recipe => recipe.id !== id)
+  }
 }
